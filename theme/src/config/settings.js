064d3f0aefeaccc6f165dab4668f936ce5aff0cd
Data88 <andrew1on1@gmail.com>
2022-11-25 13:19:04 +0200

    settings

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `wh0 | An authentication company`,
-	description: `We're making it simpler to trust`,
+	title: `wh0 | The authentication company`,
+	description: `Making trust simpler`,
 	url: `https://wh0.co.za`, // No trailing slash!
 	name: `wh0`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
